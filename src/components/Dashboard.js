7d2424f8883a1205019f98739e67af4d7cdee288
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-22 19:52:31 +0530

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,64 +1,54 @@
-import React, { useContext, useEffect ,useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginContext } from './ContextProvider/Context';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
 
 const Dashboard = () => {
-
     const { logindata, setLoginData } = useContext(LoginContext);
-
     const [data, setData] = useState(false);
-
-
     const history = useNavigate();
 
     const DashboardValid = async () => {
-        let token = localStorage.getItem("usersdatatoken");
-
-        const res = await fetch("/validuser", {
-            method: "GET",
+        let token = localStorage.getItem('usersdatatoken');
+        const res = await fetch('/validuser', {
+            method: 'GET',
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": token
-            }
+                'Content-Type': 'application/json',
+                Authorization: token,
+            },
         });
-
         const data = await res.json();
 
         if (data.status == 401 || !data) {
-            history("*");
+            history('*');
         } else {
-            console.log("user verify");
-            setLoginData(data)
-            history("/home");
+            console.log('user verify');
+            setLoginData(data);
+            history('/home');
         }
-    }
-
+    };
 
     useEffect(() => {
         setTimeout(() => {
             DashboardValid();
-            setData(true)
-        }, 2000)
-
-    }, [])
+            setData(true);
+        }, 2000);
+    }, []);
 
     return (
         <>
-            {
-                data ? <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <img src="./man.png" style={{ width: "200px", marginTop: 20 }} alt="" />
-                    <h1>User Email:{logindata ? logindata.ValidUserOne.email : ""}</h1>
-                </div> : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
-                    Loading... &nbsp;
-                    <CircularProgress />
-                </Box>
-            }
-
+            {data ? (
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <img src="./man.png" style={{ width: '200px', marginTop: 20 }} alt="" />
+                    <h1>User Email:{logindata ? logindata.ValidUserOne.email : ''}</h1>
+                </div>
+            ) : (
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    {/* <img src="./custom-loading-image.gif" style={{ width: '200px', marginTop: 20 }} alt="" /> */}
+                    <h1>Loading...</h1>
+                </div>
+            )}
         </>
+    );
+};
 
-    )
-}
-
-export default Dashboard
+export default Dashboard;
